e4376aadf5208a1f48f1af7a833981a7f8092db8
Mattias <mw222rs@student.lnu.se>
2016-03-25 10:32:42 +0100

    router startup

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,5 +1,6 @@
-import {Component} from "angular2/core";
-import {Control} from "angular2/common";
+import { Component } from "angular2/core";
+import { Control } from "angular2/common";
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from "angular2/router";
 
 import "../sass/base.scss";
 
---
+++ src/dashboard/dashboard.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "angular2/core";
+import { Router } from "angular2/router";
+
+@Component({
+  selector: "dashboard",
+  templateUrl: "./dashboard.component.html"
+})
+
+export class DashboardComponent {}
