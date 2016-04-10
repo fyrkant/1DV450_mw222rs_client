c1cb52fd4d9fd4b919165e792e33f25c5d4df3fd
Mattias <mw222rs@student.lnu.se>
2016-04-10 22:45:10 +0200

    change to hash based routes, stupid defaults...

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from "angular2/core";
 import { Control } from "angular2/common";
-import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from "angular2/router";
+import { RouteConfig
+       , Location
+       , LocationStrategy
+       , HashLocationStrategy
+       , ROUTER_DIRECTIVES
+       , ROUTER_PROVIDERS } from "angular2/router";
 
 import { DashboardComponent } from "./dashboard/dashboard";
 import { AboutComponent } from "./about/about";
@@ -10,8 +15,7 @@ import "../sass/base.scss";
 @Component({
     selector: "app",
     template: require("./app.component.html"),
-    directives: [ROUTER_DIRECTIVES],
-    providers: [ROUTER_PROVIDERS]
+    directives: [ROUTER_DIRECTIVES]
 })
 @RouteConfig([
   {
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrap } from "angular2/platform/browser";
+import { provide } from "angular2/core";
+import { RouteConfig
+       , Location
+       , LocationStrategy
+       , HashLocationStrategy
+       , ROUTER_PROVIDERS } from "angular2/router";
 import { AppComponent } from "./app.component";
 
-bootstrap(AppComponent, [])
+bootstrap(AppComponent, [
+  ROUTER_PROVIDERS,
+  provide(LocationStrategy, {useClass: HashLocationStrategy})
+])
   .then(success => console.log("Bootstrap success!"))
   .catch(error => console.log(error));
