30d51068bdacfa54ed20639fce26f3e89bedb891
Mattias <mw222rs@student.lnu.se>
2016-05-01 23:15:32 +0200

    angular2 http testing

--- src/components/login/login.component.ts
+++ src/components/login/login.component.ts
@@ -1,11 +1,13 @@
 import { Component } from "angular2/core";
-import { Http } from "angular2/http";
+import { Http, Headers, RequestOptions, Response } from "angular2/http";
 import { Router } from "angular2/router";
 import {Subject} from "rxjs/Subject";
 import {Observable} from "rxjs/Observable";
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/do";
 import "rxjs/add/observable/merge";
+
+import C from "../../constants";
 
 @Component({
   selector: "login",
@@ -17,9 +19,13 @@ export class LoginComponent {
     .map(val => val + "hello");
 
   getData() {
-    this.http.get("http://lit-river-92285.herokuapp.com/api/places")
-      .map(val => console.log(val))
-      .subscribe(val => console.log(val));
+    let headers = new Headers({ 'X-Api-key': C.API_KEY });
+    let options = new RequestOptions({ headers: headers });
+
+
+    this.http.get("http://lit-river-92285.herokuapp.com/api/places", options)
+      .map(val => val)
+      .subscribe(res => console.log(res.json()));
   }
 
   constructor (private http: Http) {
